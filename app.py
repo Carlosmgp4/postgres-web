a85bc02fb6ac3c7ef0c13bb78fc158130d4a79f1
Carlos Gámez <carlosmanuelgamezperezdeguzman@gmail.com>
2022-10-31 00:30:47 +0100

    ficheros

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, session, redirect, url_for, render_template, flash, request
+import psycopg2
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html") 
+@app.route('/login',methods=['POST'])
+def login():
+    cofra=[]
+    usuario=request.form.get("usuario")
+    contraseña=request.form.get("contraseña")
+    bd=request.form.get("bd")
+    conectar=psycopg2.connect(dbname=bd, user=usuario, password=contraseña, host="localhost")
+    cursor = conectar.cursor()
+    cursor.execute("SELECT nombrecorto FROM cofradias;")
+    cofradias = cursor.fetchall()
+    for c in cofradias:
+	cofra.append(c)
+    return render_template("login.html",cofra=cofra)
